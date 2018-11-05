87daed8a63309791b10298d972ea31e881a083d7
sjoerd20 <sjoerd.terpstra@outlook.com>
2018-11-05 13:46:56 +0100

    added main.py

--- code/load_data.py
+++ code/load_data.py
@@ -1,24 +1,8 @@
-# load data
+# loads the netlists
+def load_netlists(file_net_lists):
+    dir_netlists = '../data/' + file_net_lists
+    with open(v, 'r') as f_netlists:
 
-class Load_data():
-    def __init__(self, file_net_lists, file_chip):
-        self.file_net_lists = file_net_lists
-        self.file_chip = file_chip
-
-    # loads the netlists
-    def load_netlists(self):
-        dir_netlists = '../data/' + self.file_net_lists
-        with open(v, 'r') as f_netlists:
-
-            # write code to load the net lists from f_netlists
-            return True
-        return False
-
-    # loads chips
-    def load_chip(self):
-        dir_chip = '../data/' + self.file_chip
-        with open(dir_chip, 'r') as f_chip:
-
-            # write code to load the chip from f_chip
-            return True
-        return False
+        # write code to load the net lists from f_netlists
+        return True
+    return False
